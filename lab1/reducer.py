1a9444719a8aacb1d5fe5f9347631fa272a9b5de
tkeatkaew <thanit@rmutl.ac.th>
2025-05-24 19:04:00 +0700

    lab2 basic updated

---
+++ lab1/reducer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+from collections import defaultdict
+
+word_counts = defaultdict(int)
+
+for line in sys.stdin:
+    word, count = line.strip().split("\t")
+    word_counts[word] += int(count)
+
+for word, count in word_counts.items():
+    print(f"{word}\t{count}")
